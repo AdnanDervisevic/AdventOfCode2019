f2f6fa232cc0a3ab900b6da6affef5e8e9ce27cc
Adnan Dervisevic <adnan@adfenix.com>
2019-12-02 20:28:08 +0100

    Change to spaces cause github

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,44 @@ const getInstructionValue = (index: number) => input[input[index]]
 const getAddress = (index: number) => input[index]
 
 const handleMultiplication = (index: number) => {
-	const product = getInstructionValue(index + 1) * getInstructionValue(index + 2)
+    const product = getInstructionValue(index + 1) * getInstructionValue(index + 2)
 
-	input[getAddress(index + 3)] = product
+    input[getAddress(index + 3)] = product
 }
 
 const handleAdd = (index: number) => {
-	const sum = getInstructionValue(index + 1) + getInstructionValue(index + 2)
+    const sum = getInstructionValue(index + 1) + getInstructionValue(index + 2)
 
-	input[getAddress(index + 3)] = sum
+    input[getAddress(index + 3)] = sum
 }
 
 const handleOps = (value: number, index: number) => {
-	if (index % 4 !== 0) {
-		return
-	}
+    if (index % 4 !== 0) {
+        return
+    }
 
-	switch (value) {
-		case 1:
-			handleAdd(index)
-			break
-		case 2:
-			handleMultiplication(index)
-			break
-		case 99:
-			break
-	}
+    switch (value) {
+        case 1:
+            handleAdd(index)
+            break
+        case 2:
+            handleMultiplication(index)
+            break
+        case 99:
+            break
+    }
 }
 
 const runComputer = () => {
-	input.forEach(handleOps)
+    input.forEach(handleOps)
 
-	console.table(input) /* Debug bby */
+    console.table(input) /* Debug bby */
 
-	return input[0]
+    return input[0]
 }
 
 const App: React.FC = () => {
-	return <div>{runComputer()}</div>
+    return <div>{runComputer()}</div>
 }
 
 export default App
